d4a782f4ee52b1e52ead92a0463056792a32e836
Eunice Park <28845173+eunipa@users.noreply.github.com>
2017-07-26 04:21:31 -0700

    Random background image
    Added a random background image from Unsplash with JS.

---
+++ assets/main.js
@@ -0,0 +1,3 @@
+var randomNumber = Math.floor(Math.random() * 75);
+
+document.getElementById("unsplashbg").style.backgroundImage = "url('https://source.unsplash.com/collection/766896/${getRandomNum()}')";
